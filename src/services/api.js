679cd8903f726082d83811b3ec6c2b8c1591126a
DerandaBagas17 <111202314906@mhs.dinus.ac.id>
2026-04-18 19:00:15 +0700

    update env

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://13.211.219.19/api',
+  baseURL: import.meta.env.VITE_API_URL,
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json'
