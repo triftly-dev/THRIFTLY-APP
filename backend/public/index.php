a2a651248db11a6a3a044e3b44fe0e8cc24abf0b
triftly-dev <triftlydev@gmail.com>
2026-04-25 01:00:30 +0700

    Enable display_errors for debugging

--- backend/public/index.php
+++ backend/public/index.php
@@ -7,6 +7,10 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 define('LARAVEL_START', microtime(true));
 
 require __DIR__.'/../vendor/autoload.php';
