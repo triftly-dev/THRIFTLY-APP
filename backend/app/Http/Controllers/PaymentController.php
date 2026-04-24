6258405ff04a8eccaf58801c4fd3a71635682a22
triftly-dev <triftlydev@gmail.com>
2026-04-24 22:54:18 +0700

    Fix: Add clientKey to PaymentController

--- backend/app/Http/Controllers/PaymentController.php
+++ backend/app/Http/Controllers/PaymentController.php
@@ -19,6 +19,7 @@ class PaymentController extends Controller
         ]);
 
         Config::$serverKey = env('MIDTRANS_SERVER_KEY');
+        Config::$clientKey = env('MIDTRANS_CLIENT_KEY');
         Config::$isProduction = env('MIDTRANS_IS_PRODUCTION', false);
         Config::$isSanitized = true;
         Config::$is3ds = true;
