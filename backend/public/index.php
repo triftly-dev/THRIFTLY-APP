3b35e2bd595932abfea6c1e3509a363ca3c86ba9
triftly-dev <triftlydev@gmail.com>
2026-04-25 00:54:29 +0700

    Unblock index.php gateway

--- backend/public/index.php
+++ backend/public/index.php
@@ -7,22 +7,6 @@
  * @author   Taylor Otwell <taylor@laravel.com>
  */
 
-// JALUR TIKUS TOTAL: Paksa respon dari gerbang paling depan
-if (isset($_SERVER['REQUEST_URI']) && strpos($_SERVER['REQUEST_URI'], 'payment/token') !== false) {
-    header('Content-Type: application/json');
-    header('Access-Control-Allow-Origin: *');
-    header('Access-Control-Allow-Methods: POST, GET, OPTIONS');
-    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
-    
-    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-        http_response_code(200);
-        exit;
-    }
-
-    echo json_encode(['message' => 'GERBANG DEPAN (INDEX.PHP) TEMBUS!', 'status' => 'success']);
-    exit;
-}
-
 define('LARAVEL_START', microtime(true));
 
 require __DIR__.'/../vendor/autoload.php';
