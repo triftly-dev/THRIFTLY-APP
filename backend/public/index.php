3f89fc0bfebd491bebce0457abd32205800117ac
triftly-dev <triftlydev@gmail.com>
2026-04-25 01:15:07 +0700

    fix: remove duplicate cors headers

--- backend/public/index.php
+++ backend/public/index.php
@@ -11,16 +11,6 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// JALUR TIKUS CORS: Paksa header muncul di pintu paling depan
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization, X-Requested-With');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    http_response_code(200);
-    exit;
-}
-
 define('LARAVEL_START', microtime(true));
 
 require __DIR__.'/../vendor/autoload.php';
