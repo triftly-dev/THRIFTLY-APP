27ff1f08adb8d78c95f0f643de74e3e6e54a2c9e
DerandaBagas17 <111202314906@mhs.dinus.ac.id>
2026-04-24 17:18:35 +0700

    edit userService.js

---
+++ frontend/src/services/api.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const api = axios.create({
+  baseURL: 'http://localhost:8000/api',
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+  }
+})
+
+// Pasang Interceptor untuk selalu membawa Bearer Token (Sanctum) yang tersimpan
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem('token')
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`
+  }
+  return config
+})
+
+export default api
