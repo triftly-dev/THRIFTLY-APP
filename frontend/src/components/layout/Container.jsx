e4a96c8ced740d7d2695dde4ea3fd15f872b2dfe
triftly-dev <triftlydev@gmail.com>
2026-04-24 19:28:08 +0700

    chore: clean up repo structure for vercel

--- frontend/src/components/layout/Container.jsx
+++
@@ -1,9 +0,0 @@
-const Container = ({ children, className = '' }) => {
-  return (
-    <div className={`container mx-auto px-4 py-6 md:py-8 ${className}`}>
-      {children}
-    </div>
-  )
-}
-
-export default Container
