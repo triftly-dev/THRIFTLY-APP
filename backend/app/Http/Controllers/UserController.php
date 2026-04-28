0348da26f7f335a1c4959ee42328b7b7be76ec19
triftly-dev <triftlydev@gmail.com>
2026-04-28 21:19:59 +0700

    add fuction destroy UserController

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -33,4 +33,21 @@ class UserController extends Controller
             'user' => $user
         ]);
     }
+
+    public function destroy($id)
+    {
+        $user = User::find($id);
+
+        if (!$user) {
+            return response()->json([
+                'message' => 'User not found.'
+            ], 404);
+        }
+
+        $user->delete();
+
+        return response()->json([
+            'message' => 'User deleted successfully.'
+        ]);
+    }
 }
