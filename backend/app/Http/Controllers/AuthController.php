53d4a38615f50c610e4825dd70f3d6aff18ab3f0
triftly-dev <triftlydev@gmail.com>
2026-05-12 17:19:10 +0700

    Remove auto-verify for Google Auth - all new users must verify email

--- backend/app/Http/Controllers/AuthController.php
+++ backend/app/Http/Controllers/AuthController.php
@@ -138,7 +138,7 @@ class AuthController extends Controller
                     'password' => bcrypt(Str::random(16)),
                     'role' => 'buyer',
                 ]);
-                $user->markEmailAsVerified();
+                $user->sendEmailVerificationNotification();
             } else {
                 $user->update([
                     'google_id' => $googleUser->id,
