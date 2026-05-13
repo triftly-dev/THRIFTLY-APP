e52c37e16a111a6bfe6490a6e415a0ca8bbb4ac4
triftly-dev <triftlydev@gmail.com>
2026-05-13 17:28:00 +0700

    Fix: Remove duplicate verification.send route name

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -12,7 +12,3 @@ Route::get('/', function () {
 Route::get('/email/verify/{id}/{hash}', [AuthController::class, 'verifyEmail'])
     ->middleware(['signed', 'throttle:6,1'])
     ->name('verification.verify');
-
-Route::post('/email/verification-notification', [AuthController::class, 'resendVerificationEmail'])
-    ->middleware(['auth:sanctum', 'throttle:1,1'])
-    ->name('verification.send');
